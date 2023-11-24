518c7a204f616a3b8d3d7c2cdefdadbf8cf5f686
reinaldolds <reinaldolira8@gmail.com>
2023-11-24 13:44:39 -0300

    login.js,indexlogin,img

--- src/paginas/login.js
+++ src/paginas/login.js
@@ -5,6 +5,24 @@ function Login() {
         <div>
             <Header />
             <p>Página de login</p>
+            <div class="contem"/>
+       <header>
+         <h1>LOGIN</h1>
+        </header>
+        <form action=""/>
+            <div>
+                <label for="">Usuário:</label>
+                <input type="text" id="usuario" placeholder="Digite o nome do usuário"/><br/>
+                <label for="" id="senha" placeholder="Digite a senha">Senha:</label>
+                <input type="password" placeholder="Digite a Senha"/><br/>
+                <button type="submit">Entrar</button><br/>
+            <div/>
+        <form/><br/>
+        <a href="colocaquiRanyindex" class="cds">Cadastramento Aqui</a>
+        <div class="rec">
+            <label for="" class="final">Esqueceu a Senha?</label><h5><a href="recuperar.html">Clique Aqui</a></h5>
+        </div>
+    </div>
         </div>
     )
 }
